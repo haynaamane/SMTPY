2cd55d4f892c5843754b76412a559872f031b2a6
haytham naamane <84972118+haynaamane@users.noreply.github.com>
2021-08-28 13:12:26 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,26 @@
+#smtp server
+import sys
+import smtplib
 
+server=smtplib.SMTP("your_HOST",587)
+server.ehlo()
+server.starttls()
+
+server.login('smtp_username','smtp_password')
+
+
+try:
+	subject='naamane.me'
+	html = """\
+this is HTML body
+"""
+
+	message= 'Subject: {}\n\n{}'.format(subject,html)
+
+	server.sendmail("FROM_email",sys.argv[1],message)
+	print('[SUCCESS] '+ sys.argv[1])
+except Exception as e:
+	print('[ERR] '+ sys.argv[1])
+
+finally:
+	server.quit()
